51d7a18aaf60be97b71a9a79a2c324bc7c72df83
Grant Powell <gjamespowell@gmail.com>
2025-11-09 09:23:05 -0500

    Start building out the ty lib

--- src/ty.rs
+++ src/ty.rs
@@ -1,10 +1,23 @@
-use std::collections::HashMap;
+use smartstring::alias::String;
+use std::{collections::HashMap, sync::Arc};
 
+#[derive(PartialEq, Eq, Debug, Clone)]
 pub struct ExtendedTy {
     pub ty: Ty,
     pub nullabe: bool
 }
 
+#[derive(PartialEq, Eq, Debug, Clone)]
 pub enum Ty {
-    Struct { fields: HashMap<string, string> }
+    Int,
+    Real,
+    Double,
+    Text,
+    Boolean,
+    Float,
+    TimestampTz,
+    Bytea,
+    Jsonb,
+    Struct(HashMap<String, ExtendedTy>),
+    Record(Arc<ExtendedTy>)
 }
